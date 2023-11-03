5138ac2ce017170e16c0d00a3b785da3a8e33e43
AubreyLangan <141950977+AubreyLangan@users.noreply.github.com>
2023-11-03 15:20:23 -0700

    data upload

---
+++ moviesData.js
@@ -0,0 +1,104 @@
+const moviesData = [
+    {
+        id: 1,
+        title: "Halloween (1978)",
+        image: "../public/images/halloween.jpg",
+        synopsis: "A classic horror film about the masked killer Michael Meyers.",
+        reviews: [
+            {text: "One of the best horror movies ever made!", rating:5},
+            {text:"Scared me to death.", rating:4},
+        ],
+    },
+    {
+        id: 2,
+        title: "Saw",
+        image: "../public/images/saw.jpg",
+        synopsis: "Two men find themselves trapped in a sadistic game of life and death.",
+        reviews: [
+            {text: "A thrilling and twisted horror experience.", rating:4},
+            {text: "A gory masterpiece.", rating:4,},
+        ],
+    },
+    {
+        id: 3,
+        title: "Hocus Pocus",
+        image: "../public/images/hocus.jpg",
+        synopsis: "Three witches are resurrected in Salem on Halloween night.",
+        reviews: [
+            {text: "A family-friendly Halloween classic.", rating: 4},
+            {text: "Witches, magic, and humor - what's not to love?", rating:4},
+        ],
+    },
+    {
+        id: 4,
+        title: "Beetlejuice",
+        image: "../public/images/beetlejuice.jpg",
+        synopsis: "A recently deceased couple tries to scare away the new residents of their home with the help of a devious poltergeist.",
+        reviews: [
+            {text: "Wacky and fun - a great Halloween movie.", rating: 4},
+            {text: "Michael Keaton's Beetlejuice is iconic.", rating: 5},
+        ],
+    },
+    {
+        id: 5,
+        title: "Corpse Bride",
+        image: "../public/images/corpsebride.jpg",
+        synopsis: "A young man accidentally proposes to a deceased bride while rehearsing his wedding vows in the woods.",
+        reviews: [
+            {text: "A beautifully animated Tim Burton film.", rating: 4},
+            {text: "Dark and enchanting, a perfect Halloween choice.", rating: 4},
+        ],
+    },
+    {
+        id: 6,
+        title: "The Exorcist",
+        image: "../public/images/exorcist.jpg",
+        synopsis: "A mother seeks the help of two priests to save her daughter from demonic possession.",
+        reviews: [
+            {text: "One of the most terrifying films ever made.", rating: 5},
+            {text: "Still haunting audiences after all these years.", rating: 5},
+        ],
+    },
+    {
+        id: 7,
+        title: "The Shining",
+        image: "../public/images/theshining.jpg",
+        synopsis: "A family becomes isolated in an eerie, haunted hotel during the winter season.",
+        reviews: [
+            {text:"Jack Nicholson's performance is unforgettable.", rating: 5},
+            {text: "A masterclass in psycholo9gical horror.", rating: 5},
+        ],
+    },
+    {
+        id: 8,
+        title: "The Conjuring",
+        image: "../public/images/conjuring.jpg",
+        synopsis: "Paranormal investigators help a family terrorized by a dark prescence in their farmhouse.",
+        reviews: [
+            {text: "Scary as hell! A modern classic.", rating: 5},
+            {text: "Genuinely chilling. The jump scares get me every time.", rating: 5},
+        ],
+    },
+    {
+        id: 9,
+        title: "The Babadook",
+        image: "../public/images/babadook.jpg", 
+        synopsis: "A single mother and her child are haunted by a sinister prescence called the Babadook.",
+        reviews: [
+            {text: "A psychological horror gem.", rating: 4},
+            {text: "A great exploration of grief and fear.", rating: 4},
+        ],
+    },
+    {
+        id: 10,
+        title: "The Ring",
+        image: "../public/images/thering.jpg",
+        synopsis: "A journalist investigates a cursed videotape that kills the viewer seven days after watching it.",
+        reviews: [
+            {text: "Terrifying. It made me afraid of my TV.", rating: 4},
+            {text: "A modern ghost story that sticks with you.", rating: 4},
+        ],
+    },
+];
+
+export default moviesData;
